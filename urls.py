4a227f504c7667b8314bd067f5eaa35726d8ee0a
Brandon <jayster@protonmail.com>
2017-05-21 21:55:27 -0500

    Update urls.py

--- urls.py
+++ urls.py
@@ -20,10 +20,11 @@ Including another URLconf
 # ]
 from django.conf.urls import url, include
 from django.contrib import admin
+from rango import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('personal.urls')),
     url(r'^blog/', include('blog.urls')),
-
-]
+    url(r'^$/', views.index, name='index'),
+    url(r'^rango/', include('rango.urls')),
